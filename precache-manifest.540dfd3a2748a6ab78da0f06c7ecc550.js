95da6c87aa8c8cbc07faaea2babd3728ca4c21f5
csy <13540039093@163.com>
2020-07-22 19:31:03 +0800

    deploy

---
+++ precache-manifest.540dfd3a2748a6ab78da0f06c7ecc550.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a3d2bcf37ce0ccccd4",
+    "url": "/money-record-website/css/app.1d0ee8ed.css"
+  },
+  {
+    "revision": "651d32ed59e37ece628f",
+    "url": "/money-record-website/css/chunk-vendors.5ad50e34.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/money-record-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/money-record-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "1ece1e4a06065c600e9e2464ca64f144",
+    "url": "/money-record-website/index.html"
+  },
+  {
+    "revision": "b1a3d2bcf37ce0ccccd4",
+    "url": "/money-record-website/js/app.aaf7a9ee.js"
+  },
+  {
+    "revision": "651d32ed59e37ece628f",
+    "url": "/money-record-website/js/chunk-vendors.47c256f8.js"
+  },
+  {
+    "revision": "1cc3de9118129c721924cfcbb35467aa",
+    "url": "/money-record-website/manifest.json"
+  },
+  {
+    "revision": "658a9b2e200b0c3b4784116b7a5a6a2a",
+    "url": "/money-record-website/qrCode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-record-website/robots.txt"
+  }
+]);
